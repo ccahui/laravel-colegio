241b495c8b58e6c72fcc672cf050a0c25c33501b
ccahui <kris_ti4n@hotmail.com>
2018-10-06 00:23:49 -0500

    Primeros Pasos en Laravel

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,29 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/alumnos',function(){
+    return "alumnos.html";
+});
+Route::get('/alumnos/grado-{id}',function($id){
+    return "alumnos-grado.html: {$id}";
+}) -> where('id','[1-6]');
+
+Route::get('/alumnos/ver-{id}',function($id){
+    return "alumnos-ver.html: {$id}";
+}) -> where('id','[0-9]+');
+
+Route::get('/alumnos/editar-{id}',function($id){
+    return "alumnos-editar.html: {$id}";
+}) -> where('id','[0-9]+');
+
+Route::get('/alumnos/eliminar-{id}',function($id){
+    return "Eliminando: {$id}";
+}) -> where('id','[0-9]+');
+
+Route::get('/alumnos/crear',function(){
+    return "alumnos-crear.html";
+});
+
+Route::get('/iniciar-sesion',function(){
+   return "iniciar-session.html"; 
+});
---
+++ tests/Feature/ModuloAlumnosTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ModuloAlumnosTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+    /**
+     * @test
+     */
+    public function lista_de_grados(){
+        $this -> get('/alumnos')
+                ->assertStatus(200);
+    }
+    /**
+     * @test
+     */
+    public function lista_de_alumnos_del_grado(){
+        $this -> get('/alumnos/grado-5')
+                ->assertStatus(200);
+    }
+    /**
+     * @test
+     */
+    public function editar_un_alumno(){
+        $this -> get('/alumnos/editar-5')
+                ->assertStatus(200);
+    }
+    /**
+     * @test
+     */
+    public function ver_un_alumno(){
+        $this -> get('/alumnos/ver-5')
+                ->assertStatus(200);
+    }
+    /**
+     * @test
+     */
+    public function eliminar_un_alumno(){
+        $this -> get('/alumnos/eliminar-5')
+                ->assertStatus(200);
+    }
+    /**
+     * @test
+     */
+    public function crear_un_alumno(){
+        $this -> get('/alumnos/crear')
+                ->assertStatus(200);
+    }
+    
+}
